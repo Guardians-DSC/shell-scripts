cb7b18443631398e65c3707aef504bbb0b31c6c4
ana29 <godoyisadora@gmail.com>
2017-04-27 20:11:58 -0300

    Script de instalação do GHC

---
+++ packages/ghc/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instala√ß√ £oGHC                                                                                                                        # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install
+apt-get -y install ghc
