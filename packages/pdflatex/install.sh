695dd0fd9a8174b04027a5a99b1bf350f3295628
ana29 <godoyisadora@gmail.com>
2017-04-27 19:40:13 -0300

    Script de instalação do pdflatex

---
+++ packages/pdflatex/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instalaçãopdflatex                                                                                                                        # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install
+#apt-get -y install pdflatex
+apt-get -y install texlive-latex-base
