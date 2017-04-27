c49044c9ae749a279936a89499e8c10c18994790
ana29 <godoyisadora@gmail.com>
2017-04-27 17:13:57 -0300

    Script de instalação do Python3

--- packages/python3/install.sh
+++ packages/python3/install.sh
@@ -2,31 +2,14 @@
 
 #################################################################################################
 #                                                                                                                                                                                       #
-# Script de instala√ao Alloy                                                           
-                                                     # #
+# Script de instala√√o  Python 3                                                                                                                        # #
 # Autores: Ana Godoy - godoyisadora@gmail.com      #
 # Abril de 2017                                                                                                                                                       #
 #                                                                                                                                                                                       #
 #################################################################################################
 
 #Update
-apt-get -y update
+apt-get update
 
-#Install Java6
-apt-get -y install software-properties-common python-software-properties
-apt-get -y  update
-apt-get -y install default-jre
-apt-get -y install default-jdk
-yes '' | add-apt-repository ppa:webupd8team/java
-apt-get -y update
-apt-get -y install oracle-java6-installer
-
-#Download Alloy
-wget http://alloy.mit.edu/alloy/downloads/alloy4.2.jar
-
-#Create soft link to  Alloy
-from=/scripts/packages/alloy
-to=/usr/local/bin
-ln -s $from  $to
-
-
+#Install Python 2.7
+apt-get -y install python3
