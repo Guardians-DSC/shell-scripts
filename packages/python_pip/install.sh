d44453e63ba292a63925a87a4640a8028ff95d3a
ana29 <godoyisadora@gmail.com>
2017-04-27 16:45:01 -0300

    Script de instalação do Python-pip

---
+++ packages/python_pip/install.sh
@@ -0,0 +1,14 @@
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instala��o Pytpip
+                                                     # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install
+apt-get -y install python-pip
