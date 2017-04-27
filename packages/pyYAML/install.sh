c74bc801482c995e916feb18c63244c8a0c61aaa
ana29 <godoyisadora@gmail.com>
2017-04-27 17:04:41 -0300

    Script de instalação do PyYAML e update do python-pip

--- packages/pyYAML/install.sh
+++ packages/pyYAML/install.sh
@@ -1,6 +1,6 @@
 #################################################################################################
 #                                                                                                                                                                                       #
-# Script de instalaï¿½ï¿½o Pytpip
+# Script de instalaï¿½ïPyYAML
                                                      # #
 # Autores: Ana Godoy - godoyisadora@gmail.com      #
 # Abril de 2017                                                                                                                                                       #
@@ -10,6 +10,9 @@
 #Update
 apt-get -y update
 
-#Install
+#Install pip
 apt-get -y install python-pip
 pip install --upgrade pip
+
+#Install PyYaml
+pip install pyyaml
