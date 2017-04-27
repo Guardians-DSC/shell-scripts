0e8e122dab2e4e265f91a17c1096710244510500
ana29 <godoyisadora@gmail.com>
2017-04-27 19:16:53 -0300

    Script de instalação do  apt-cache show libxml2-dev

---
+++ packages/libxml2_dev/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instalaï¿½ï¿  libxml2-dev                                                                                                                                   # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install
+apt-get -y install libxml2
+apt-get -y install libxml2-dev
+
