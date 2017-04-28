90d4cadc34abfdcfd173a8aeadb691fc18334ee1
ana29 <godoyisadora@gmail.com>
2017-04-28 16:07:39 -0300

    Script de instalação do SWI-Prolog

---
+++ packages/swi_prolog/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instalaçãoswi-prolog                                                                                                                         # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install
+apt-get -y install swi-prolog
+
+
