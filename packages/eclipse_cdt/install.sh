1c97b4611e4a453e6879a90f77f3a50dc8195fab
ana29 <godoyisadora@gmail.com>
2017-04-29 11:04:02 -0300

    Script de instalação do Eclipse CDT

---
+++ packages/eclipse_cdt/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instalaï¿½ïEclipse CDT
+                                                     # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install
+apt-get -y install eclipse eclipse-cdt g++
