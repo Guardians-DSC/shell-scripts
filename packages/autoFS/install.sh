19a403fd6e699dc2bb3e788d22eefa8bd3528652
root <root@lcc2-15.lcc.ufcg.edu.br>
2017-05-02 10:23:50 -0300

    Script de instalação do autoFs

---
+++ packages/autoFS/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instalaï¿   autoFs                                                           
+                                                     # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install 
+apt-get -y install autofs
+
+#Config
+#https://help.ubuntu.com/community/Autofs
