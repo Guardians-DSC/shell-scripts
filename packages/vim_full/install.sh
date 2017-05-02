4afe28b3818d7772bb207d79d2dbd42fedccc524
root <root@lcc2-15.lcc.ufcg.edu.br>
2017-05-02 09:31:34 -0300

    Script de instalação do vim-full

---
+++ packages/vim_full/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instalaï¿½ïvim-full                                                           
+                                                     # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install vim-full
+apt-get -y install vim vim-scripts vim-doc vim-latexsuite vim-gui-common vim-gnome
