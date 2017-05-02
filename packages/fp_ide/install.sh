3b6162f3fc7816d0a0382e16375c51bb7bd219ba
root <root@lcc2-15.lcc.ufcg.edu.br>
2017-05-02 09:11:44 -0300

    Script de instalação do fp-ide

---
+++ packages/fp_ide/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instala√√o  Fp-ide                                                      
+                                                     # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install Fp
+apt-get -y install fp-compiler
+
+#Install Fp-ide
+apt-get -y install fp-ide
+
