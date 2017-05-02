df74401a4df19987b5fa45d9384b2309009bfbfb
root <root@lcc2-15.lcc.ufcg.edu.br>
2017-05-02 10:58:38 -0300

    Script de instalação do Sublime

---
+++ packages/sublime/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instala��o Sublime                                                        
+                                                     # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install Sublime
+apt-get -y install software-properties-common python-software-properties
+apt-get -y  update
+yes '' | add-apt-repository ppa:webupd8team/sublime-text-3
+apt-get -y install sublime-text-installer
+
+ln -s /opt/sublime_text/sublime_text /usr/local/bin/subl
+
