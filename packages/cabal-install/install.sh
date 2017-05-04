7f4f3e5d9c1d489abb0b57d87385d3b5ab9bad42
Ana <godoyisadora@gmail.com>
2017-05-04 15:13:45 -0300

    Cabal finalizado

--- packages/cabal-install/install.sh
+++ packages/cabal-install/install.sh
@@ -11,6 +11,9 @@
 #Update
 apt-get -y update
 
+apt-get -y install gcc g++
+apt-get install zlib1g-dev
+
 #Install Cabal
 apt-get -y install software-properties-common python-software-properties
 yes ' ' |add-apt-repository -y ppa:hvr/ghc
@@ -23,8 +26,10 @@ export PATH=~/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:$PATH
 
 wget http://hackage.haskell.org/package/cabal-install-1.24.0.2/cabal-install-1.24.0.2.tar.gz
 tar -vzxf cabal-install-1.24.0.2.tar.gz
+cd cabal-install-1.24.0.2
 ./bootstrap.sh
 EXTRA_CONFIGURE_OPTS="" ./bootstrap.sh
 export PATH=$HOME/.cabal/bin:$PATH
+cd ..
 rm cabal-install-1.24.0.2.tar.gz
-rm -r cabal-install-1.24.0.2
+#rm -r cabal-install-1.24.0.2
