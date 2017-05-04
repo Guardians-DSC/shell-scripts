76553f9e6ab701bf3f96190042c4dac5db49901b
root <root@lcc2-15.lcc.ufcg.edu.br>
2017-05-04 15:06:47 -0300

    android S - com erros

--- packages/android_studio/install.sh
+++ packages/android_studio/install.sh
@@ -12,19 +12,31 @@
 #Update
 apt-get -y update
 
-#Install Java6
-apt-get -y install software-properties-common python-software-properties
-apt-get -y  update
-apt-get -y install default-jre
-apt-get -y install default-jdk
-yes '' | add-apt-repository ppa:webupd8team/java
-apt-get -y update
-apt-get -y install java-common oracle-java8-installer
-apt-get -y install oracle-java8-set-default
-source /etc/profile
+#Install Java
+echo "===> add webupd8 repository..."  && \
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list  && \
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list  && \
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886  && \
+apt-get update  && \
+\
+\
+echo "===> install Java"  && \
+echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections  && \
+echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections  && \
+DEBIAN_FRONTEND=noninteractive  apt-get install -y --force-yes oracle-java8-installer oracle-java8-set-default  && \
+\
+\
+echo "===> clean up..."  && \
+rm -rf /var/cache/oracle-jdk8-installer  && \
+apt-get clean  && \
+rm -rf /var/lib/apt/lists/*
 
 #Install Android Studio
-yes '' | add-apt-repository ppa:maarten-fonville/android-studio
+
+apt-get -y install software-properties-common python-software-properties
+apt-get -y  update
+yes '' | add-apt-repository ppa:paolorotolo/android-studio
 apt-get -y update
-apt-get -y install android-studio
+apt-get -y install --allow-unauthenticated android-studio
+
 
