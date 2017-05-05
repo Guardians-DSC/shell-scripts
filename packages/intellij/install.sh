44bd6f9087398389689a9af7d07198cb50fe8b34
root <tainahemmanuele@gmail.com>
2017-05-05 16:11:45 -0300

    atualização de links do intellij

--- packages/intellij/install.sh
+++ packages/intellij/install.sh
@@ -12,16 +12,16 @@
 apt-get -y update && apt-get install $(cat depedences) && rm -rf /var/lib/apt/lists/*
 
 #Faz download do intellij
-wget https://download.jetbrains.com/idea/ideaIU-2016.3.3.tar.gz
+wget https://download.jetbrains.com/idea/ideaIU-2017.1.2.tar.gz
 
 #Descompacta intellij para a pasta /opt/
-tar -xf  ideaIU-2016.3.3.tar.gz -C /opt/
+tar -xf  ideaIU-2017.1.2.tar.gz -C /opt/
 
 #Entra na pasta bin dentro de /opt/
-cd /opt/idea-IU-163.11103.6/bin
+cd /opt/idea-IU-171.4249.39/bin
 
 #Executa o script de instalação do intellij
 ./idea.sh
 
 #Apaga o arquivo de download
-rm -rfv ideaIU-2016.3.3.tar.gz
+rm -rfv ideaIU-2017.1.2.tar.gz
