33b0d9c451e73096a092b848cb5fd2190b7cc46d
ana29 <godoyisadora@gmail.com>
2017-05-05 19:49:02 -0300

    Script de instalação do Visual Studio Code

--- packages/visual_studio_code/install.sh
+++ packages/visual_studio_code/install.sh
@@ -15,7 +15,8 @@ apt-get -y update
 #Install Visual Studio Code
 apt-get -y install software-properties-common python-software-properties
 apt-get -y  update
-yes '' | add-apt-repository -y "deb https://packages.microsoft.com/repos/vscode stable main"
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
-apt-get-y update
-apt-get -y install code
+yes '' | add-apt-repository ppa:ubuntu-desktop/ubuntu-make
+apt-get -y update
+apt-get -y install ubuntu-make
+
+umake web visual-studio-code
