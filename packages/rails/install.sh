58f56bce99ed8e2c29c111f0ef6dc7fa453b87cb
liraop <lirapdo@gmail.com>
2017-05-06 11:39:23 -0300

    added ruby and rails packages

---
+++ packages/rails/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#########################################################
+#                                                       #
+# author: pedro lira - pedulira@gmail.com               #
+#                                                       #
+#########################################################
+
+### update repositories and install package
+apt-get update
+
+### gem rails
+apt-get install ruby-rails
---
+++ packages/ruby/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#########################################################
+#                                                       #
+# author: pedro lira - pedulira@gmail.com               #
+#                                                       #
+#########################################################
+
+### update repositories and install package
+apt-get update && apt-get install -y $(cat dependencies) && rm -rf /var/lib/apt/lists/*
+
+### install ruby
+apt-get install ruby -y
