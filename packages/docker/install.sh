62629f5b83bea69621dee294e52bc1b17f649674
liraop <root@lcc2-12.lcc.ufcg.edu.br>
2017-05-06 10:20:59 -0300

    hotfix

--- packages/docker/install.sh
+++ packages/docker/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cd /opt/
 wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
+
 yes | apt-get update
 yes | apt-get -f install
 yes | dpkg -i docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
+
 rm docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
