1c7f4b094df960d482218faf09ebc7030b183984
liraop <lirapdo@gmail.com>
2017-05-06 10:38:59 -0300

    added vlc

---
+++ packages/vlc/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#########################################################
+#                                                       #
+# author: pedro lira - pedulira@gmail.com               #
+#                                                       #
+#########################################################
+
+### update repositories
+apt-get update
+
+### install vlc
+apt-get install vlc browser-plugin-vlc
