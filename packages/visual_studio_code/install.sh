37010657ff2eea8629d4d3962319d951463ddd8e
kaiokassiano <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2017-05-06 10:55:24 -0300

    consertando script do vscode

--- packages/visual_studio_code/install.sh
+++ packages/visual_studio_code/install.sh
@@ -2,21 +2,16 @@
 
 #################################################################################################
 #                                                                                                                                                                                       #
-# Script de instalaï¿½ï   Visual Studio Code                                                  
+# Script de instalaï¿½ï¿½   Visual Studio Code
                                                      # #
 # Autores: Ana Godoy - godoyisadora@gmail.com      #
 # Abril de 2017                                                                                                                                                       #
 #                                                                                                                                                                                       #
 #################################################################################################
 
-#Update
-apt-get -y update
+wget https://go.microsoft.com/fwlink/?LinkID=760868
 
-#Install Visual Studio Code
-apt-get -y install software-properties-common python-software-properties
-apt-get -y  update
-yes '' | add-apt-repository ppa:ubuntu-desktop/ubuntu-make
-apt-get -y update
-apt-get -y install ubuntu-make
+yes | apt-get update
+yes | dpkg -i index.html?LinkID=760868; yes | apt-get -f install; yes| dpkg -i index.html?LinkID=760868
 
-umake web visual-studio-code
+rm index.html?LinkID=760868
