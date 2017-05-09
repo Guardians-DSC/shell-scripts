c58076c58dc8e6d59b4736ad65a873dc351f8f81
liraop <lirapdo@gmail.com>
2017-05-09 17:59:57 -0300

    intellij fixed

--- packages/intellij/install.sh
+++ packages/intellij/install.sh
@@ -12,28 +12,27 @@
 apt-get -y update && apt-get install $(cat depedences) && rm -rf /var/lib/apt/lists/*
 
 #Faz download do intellij
-wget https://download.jetbrains.com/idea/ideaIU-2017.1.2.tar.gz --output-document=intellij.tar.gz
+#wget https://download.jetbrains.com/idea/ideaIU-2017.1.2.tar.gz --output-document=intellij.tar.gz
 
 #Descompacta intellij para a pasta temporaria
-tar -xf  intellij.tar.gz -C ./intellij/
+mkdir ./intellij
+tar -xzvf  intellij.tar.gz -C ./intellij/
 
 ### moving package to /opt
 mv ./intellij/* intellij/ij
 mv ./intellij/ij /opt/intellij
 
-### executa instalavel
-sh /opt/intellij/bin/idea.sh
-
-#Apaga o arquivo de download
-rm -rfv intellij.tar.gz
-
 ### grant access to all
-chmod +x /opt/intellij/bin/.sh
+chmod +x /opt/intellij/bin/idea.sh
 
 ### creating soft link
-from=/opt/intellij/bin/intellij
+from=/opt/intellij/bin/idea
 to=/usr/local/bin
 ln -s $from $to
 
 #Create Desktop Icon
 cp ./desktop-icon/intellij.desktop /usr/share/applications/
+
+#Apaga o arquivo de download
+rm -rfv intellij.tar.gz
+rm -rf ./intellij
