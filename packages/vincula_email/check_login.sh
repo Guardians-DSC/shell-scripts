0502d9c3405e70d3a7ed4ec74c87e43fa4c9f21b
samuelndm <samuel.moura@ccc.ufcg.edu.br>
2017-07-20 23:20:56 -0300

    scripts de vinculacao do email quase terminados

--- packages/vincula_email/check_login.sh
+++ packages/vincula_email/check_login.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 
-# variavel com o PATH do CSV
-file="/opt/user_check/usuarios_cadastrados.csv"
+#csv_file="/tmp/usuarios_cadastrados.csv"
 
-# checa se o arquivo existe, se não, é criado um arquivo CSV com a coluna "login" e "email"
-#if [ ! -e "$file" ]; then
-#	echo "login,email" > $file
-#	chmod 777 usuarios_cadastrados.csv
-#fi
+wget http://150.165.54.41/usuarios_cadastrados.csv -q /tmp/usuarios_cadastrados.csv
 
-# salvando o separador atual em uma variavel
 OLD_IFS=$IFS
-
-# atribuindo a virgula como novo separador
 IFS=","
 	
-# salvando na variavel o usuario logado no momento
 LOGIN_USER=$(whoami)
-
-# variavel para definir se o usuario foi encontrado ou não
 USER_FOUND="false"
 
-# lendo as linhas da coluna 'login' no arquivo CSV 
 while read login email; do
-
-	# verifica se o login do usuario atual esta contido no arquivo CSV
 	if [ "$LOGIN_USER" = "$login" ]; then
 		USER_FOUND="true"
 		break
 	fi
-done < $file
+done < usuarios_cadastrados.csv
 	
 IFS=$OLD_IFS
+rm usuarios_cadastrados.csv
 
-# se o usuario ainda nao cadastrou o email, executa o script de vinculacao do email
+# se o usuario nao estiver no arquivo .csv, o script vincula_email é executado
 if [ "$USER_FOUND" = "false" ]; then
-	gnome-terminal --maximize -e /opt/user_check/vincula_email.sh
+	gnome-terminal --maximize -e ./vincula_email.sh
 fi
--- packages/vincula_email/vincula_email.sh
+++ packages/vincula_email/vincula_email.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-valida_email() {
-	regex=^[a-z]+\\.[a-z]+@"ccc.ufcg.edu.br"
-	email=$1
 
-	if [[ $email =~ $regex ]]; then  
+valida_email() {
+	regex=^[a-z.]+@"ccc.ufcg.edu.br"
+	email=$1
+	
+	if = [ -z $email ]; then
+		echo "not ok"
+	elif [[ $email =~ $regex ]]; then  
         	echo "ok"
 	else
         	echo "not ok"
@@ -32,34 +35,67 @@ input_email() {
 	0 0
 }
 
+input_password() {
+	dialog \
+	--stdout \
+	--no-cancel \
+	--ok-label "Ok" \
+	--title "Senha" \
+	--passwordbox "" \
+	0 0
+}
+
+create_mount_point(){
+	mount_point="/mnt/montagem-emails"
+	if [ -e "$mount_point" ]; then
+		fusermount -u $mount_point
+		rm rmdir $mount_point
+	else
+		mkdir $mount_point
+	fi
+}
+
 usuario_ok="false"
 msg_box "Gostariamos de vincular sua conta do LCC ao email do curso, poderia nos informar o seu email @ccc...?"
 while true; do
 	while true; do
-		email=$(input_email "Digite seu email:")
-		email_valido=$(valida_email $email)
+                email=$(input_email "Digite seu email:")
+                email_valido=$(valida_email $email)
 		
-		if [ "$email_valido" = "ok" ]; then
-			break
-		else
-			msg_box "Email invalido -- @ccc.ufcg.edu.br -- requerido"
-		fi
-	done	
+		[ $? -eq 255 ] && exit
+
+
+                if [ "$email_valido" = "ok" ]; then
+                        break
+                else
+                        $(msg_box "Email invalido -- @ccc.ufcg.edu.br -- requerido")
+                fi
+
+        done
+
 	email_confirm=$(input_email "Digite novamente seu email:")
- 		
+	[ $? -eq 255 ] && exit 
+		
 	if [ "$email" = "$email_confirm" ]; then
 		usuario_ok="true"
 		break
 	else
-		msg_box "Os emails informados não sao identicos, digite novamente"
+		msg_box "Os emails informados nao sao identicos, digite novamente"
 	fi
-	
-	[ $? -eq 255 ] && break
 done
 msg_box "Obrigado :)" && clear
 
 if [ "$usuario_ok" = "true" ]; then
-	arq_csv="/opt/user_check/usuarios_cadastrados.csv"
+	create_mount_point
+	path_server="/opt/emails_cadastrados"
+	path_local="/mnt/montagem-emails"
+	file_local="/mnt/montagem-emails/usuarios_cadastrados.csv"
 	login=$(whoami)
-	echo "$login","$email" >> $arq_csv
-fi
+	password=$(dialog --stdout --no-cancel --ok-label "Ok" --title "Senha" --passwordbox "" 0 0)
+
+	echo $password | sshfs -p 23456 samuelndm@chopper.lcc.ufcg.edu.br:$path_server $path_local -o password_stdin
+	echo $login,$email >> $file_local
+	
+	fusermount -u $path_local
+	rmdir $path_local
+fi	
