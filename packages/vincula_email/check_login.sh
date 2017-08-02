ff68ea37158258413567a723f5753b63d1512953
samuelndm <samuel.moura@ccc.ufcg.edu.br>
2017-08-02 12:20:56 -0300

    ascripts 2.0

--- packages/vincula_email/check_login.sh
+++ packages/vincula_email/check_login.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
-#csv_file="/tmp/usuarios_cadastrados.csv"
 login=$(whoami)
 
 if [ "$login" = "root" ] || [ "$login" = "guardians" ]; then
 	exit
 fi
 
-wget http://150.165.54.41/usuarios_cadastrados.csv -q /tmp/usuarios_cadastrados.csv
+file="/home/$login/.email_aluno"
 
-OLD_IFS=$IFS
-IFS=","
-	
-LOGIN_USER=$(whoami)
-USER_FOUND="false"
+if [ ! -f "$file" ]; then
+	touch $file
+fi
 
-while read login email; do
-	if [ "$LOGIN_USER" = "$login" ]; then
-		USER_FOUND="true"
-		break
-	fi
-done < usuarios_cadastrados.csv
-	
-IFS=$OLD_IFS
-rm usuarios_cadastrados.csv
-
-# se o usuario nao estiver no arquivo .csv, o script vincula_email é executado
-if [ "$USER_FOUND" = "false" ]; then
+if [ -s "$file" ]; then
+	exit
+else
 	gnome-terminal --maximize -e /opt/vincula_email/vincula_email.sh
 fi
--- packages/vincula_email/vincula_email.sh
+++ packages/vincula_email/vincula_email.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+####################################################
+
 
 valida_email() {
 	regex=^[a-z.]+@"ccc.ufcg.edu.br"
@@ -33,25 +35,17 @@ input_email() {
 	0 0
 }
 
-create_mount_point(){
-	login=$(whoami)
-	mount_point="/mnt_alunos/montagem-emails"
-	if [ -e "$mount_point" ]; then
-		fusermount -u $mount_point
-		rm -r $mount_point
-	else
-		mkdir $mount_point
-	fi
-}
+####################################################
+
 
 usuario_ok="false"
+
 msg_box "Gostariamos de vincular sua conta do LCC ao email do curso, poderia nos informar o seu email @ccc...?"
+
 while true; do
 	while true; do
                 email=$(input_email "Digite seu email:")
                 email_valido=$(valida_email $email)
-		
-#		[ $? -eq 255 ] && exit
 
 
                 if [ "$email_valido" = "ok" ]; then
@@ -63,7 +57,6 @@ while true; do
         done
 
 	email_confirm=$(input_email "Digite novamente seu email:")
-#	[ $? -eq 255 ] && exit 
 		
 	if [ "$email" = "$email_confirm" ]; then
 		usuario_ok="true"
@@ -73,39 +66,9 @@ while true; do
 	fi
 done
 
-while true; do
-	password=$(dialog --stdout --no-cancel --ok-label "Ok" --title "Senha" --passwordbox "Poderia inserir a senha de seu usuario-LCC?" 0 0)
-	login=$(whoami)
-	path_log="/tmp/log_vincula_email"
-	
-	if [ -e "$path_log" ]; then
-		rm $path_log
-	fi
-	
-	sshpass -p $password ssh -p 23456 $login@chopper.lcc.ufcg.edu.br exit 2>$path_log
-	permission_denied=$(cat $path_log | grep "denied")
-	
-	if [ -n "$permission_denied" ]; then
-		msg_box "Ops, senha incorreta! :("
-	else
-		rm $path_log
-		break
-	fi
-done
-
-
 if [ "$usuario_ok" = "true" ]; then
-	create_mount_point
-	path_server="/opt/emails_cadastrados"
-	path_local="/mnt_alunos/montagem-emails"
-	file_local="$path_local/usuarios_cadastrados.csv"
-	
-	echo $password | sshfs -p 23456 $login@chopper.lcc.ufcg.edu.br:$path_server $path_local -o password_stdin
-	echo $login,$email >> $file_local
-	
-	password=""
-	fusermount -u $path_local
-	rmdir $path_local
+	login=$(whoami)
+	echo $login,$email >> /home/$login/.email_aluno
 fi
 msg_box "Obrigado :)"
 exit
