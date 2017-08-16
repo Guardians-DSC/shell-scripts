990b4fcf103763b7a3af4c34ec72f47abcd10edb
samuelndm <samuel.moura@ccc.ufcg.edu.br>
2017-08-16 19:24:00 -0300

    script lista_arquivos 1.3

--- scripts_lcc/lista_arquivos.sh
+++ scripts_lcc/lista_arquivos.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 user=$(whoami)
+home_user="/home/$user"
+downloads_user="$home_user/Downloads"
+documentos_user="$home_user/Documentos"
+imagens_user="$home_user/Imagens"
+videos_user="$home_user/Videos"
+lixeira_user="$home_user/.local/share/Trash/files"
+	
+escolhidos=$(du -sh $downloads_user/* $documentos_user/* $imagens_user/* $videos_user/* $lixeira_user/* $home_user/* $home_user/.??* 2>/dev/null | sort -hr | zenity   --list \
+			--title "Arquivos/Pastas" \
+			--text "Selecione o(s) arquivo(s) ou pasta(s) que deseja excluir" \
+			--width 640 \
+			--height 580 \
+			--multiple \
+			--column "Tamanho  -  Arquivos/Pastas" \
+			--separator " /")
 
-files=$(du -sh /home/$user/* /home/$user/.??* 2>/dev/null | sort -hr | zenity --list \
-	--title "Diretorio/Arquivos" \
-	--text "Selecione o que deseja excluir" \
-	--width 640 \
-	--height 480 \
-	--column "#" \
-	--column "Espaco - Arquivos" \
-	--checklist \
-	--separator " /")
-
-for file in $files; do
-	echo $("$file" | grep "/home/")
+for file in $escolhidos; do
+	if [[ "$file" =~ "/home/" ]]; then
+		echo $file
+		#rm -Rf $file
+	fi
 done
