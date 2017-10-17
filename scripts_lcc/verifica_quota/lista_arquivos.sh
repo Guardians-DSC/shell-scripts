af8a276b1848ef79fc3e0a962192d013719ca94c
samuelndm <samuel.moura@ccc.ufcg.edu.br>
2017-10-17 11:46:31 -0300

    alguns ajustes num script ae

--- scripts_lcc/verifica_quota/lista_arquivos.sh
+++ scripts_lcc/verifica_quota/lista_arquivos.sh
@@ -8,17 +8,23 @@ imagens_user="$home_user/Imagens"
 videos_user="$home_user/Videos"
 lixeira_user="$home_user/.local/share/Trash/files"
 	
-escolhidos=$(du -sh $downloads_user/* $documentos_user/* $imagens_user/* $videos_user/* $lixeira_user/* $home_user/* $home_user/.??* 2>/dev/null | sort -hr | zenity  --list \
+escolhido=$(du -sh $downloads_user/* $documentos_user/* $imagens_user/* $videos_user/* $lixeira_user/* $home_user/* $home_user/.??* 2>/dev/null | sort -hr | zenity  --list \
 			--title "Arquivos/Pastas" \
 			--text "Selecione o(s) arquivo(s) ou pasta(s) que deseja excluir" \
 			--width 640 \
 			--height 580 \
-			--multiple \
 			--column "Tamanho  -  Arquivos/Pastas" \
-			--separator " /")
+			--separator " /home")
 
-for file in $escolhidos; do
-	if [[ "$file" =~ "/home/" ]]; then
-		rm -Rf $file
+cont=0
+file=""
+for string in $escolhido; do
+	if [[ $cont -eq 1 ]]; then
+		file+=$string
+	elif [[ $cont -gt 1 ]]; then
+		file+="\ "$string
 	fi
+	cont=$cont+1
 done
+echo $file
+rm -f $file
