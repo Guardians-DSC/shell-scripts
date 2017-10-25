aeb57c20ccc9213761e3084017dd0c27b613993f
samuelndm <samuel.moura@ccc.ufcg.edu.br>
2017-10-25 12:30:50 -0300

    fix remove-arquivos

--- scripts_lcc/verifica_quota/lista_arquivos.sh
+++ scripts_lcc/verifica_quota/lista_arquivos.sh
@@ -7,8 +7,9 @@ documentos_user="$home_user/Documentos"
 imagens_user="$home_user/Imagens"
 videos_user="$home_user/Videos"
 lixeira_user="$home_user/.local/share/Trash/files"
+cache_user="$home_user/.cache/"
 	
-escolhido=$(du -sh $downloads_user/* $documentos_user/* $imagens_user/* $videos_user/* $lixeira_user/* $home_user/* $home_user/.??* 2>/dev/null | sort -hr | zenity  --list \
+escolhido=$(du -sh $downloads_user/* $documentos_user/* $imagens_user/* $videos_user/* $lixeira_user/* $cache_user/* $home_user/* $home_user/.??* 2>/dev/null | sort -hr | zenity  --list \
 			--title "Arquivos/Pastas" \
 			--text "Selecione o(s) arquivo(s) ou pasta(s) que deseja excluir" \
 			--width 640 \
@@ -16,15 +17,15 @@ escolhido=$(du -sh $downloads_user/* $documentos_user/* $imagens_user/* $videos_
 			--column "Tamanho  -  Arquivos/Pastas" \
 			--separator " /home")
 
+file=
 cont=0
-file=""
 for string in $escolhido; do
 	if [[ $cont -eq 1 ]]; then
 		file+=$string
 	elif [[ $cont -gt 1 ]]; then
-		file+="\ "$string
+		file+=" "$string
 	fi
 	cont=$cont+1
 done
-echo $file
-rm -f $file
+
+rm -Rf "$file"
