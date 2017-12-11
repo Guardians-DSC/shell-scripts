9319b601e76a7329996e8a7581600244f686f827
samuelndm <samuel.moura@ccc.ufcg.edu.br>
2017-12-11 17:00:10 -0300

    Add alteracao de limite pelo usuario

--- scripts_lcc/verifica_quota/aviso_usuario.sh
+++ scripts_lcc/verifica_quota/aviso_usuario.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source lista_arquivos.sh
+source ./lista_arquivos.sh
+source ./verifica_quota.sh
+
 
 verifica_status_aviso_usuario() {
 	
@@ -14,22 +16,26 @@ verifica_status_aviso_usuario() {
 		
 		3) apagar_cache ;;
 		
-		4) ;;
+		4) alterar_limite ;;
 
 	esac
 }
 
+
 apagar_lixeira() {
-	rm -Rf $lixeira_user && zenity --info --text="Lixeira Deletada com Sucesso."; main_aviso_usuario
+	rm -Rf $lixeira_user && zenity --info --title="Aviso" --text="Lixeira Deletada com Sucesso."; main_aviso_usuario
 }
+
 
 apagar_cache() {
 	yad --question \
+		--center \
+		--title="Aviso" \
 		--text="<big>\n Tem certeza que deseja prosseguir?\n\n O Cache dos seus navegadores \n e outros programas serão deletados.</big>"
 	
 	case $? in
 		
-		0) rm -Rf $cache_user && zenity --info --text="Cache Deletado com Sucesso."; main_aviso_usuario ;;
+		0) rm -Rf $cache_user && zenity --info --text="Cache Deletado com Sucesso." --title="Aviso"; main_aviso_usuario ;;
 		
 		1) main_aviso_usuario ;;
 	
@@ -37,17 +43,57 @@ apagar_cache() {
 }
 
 
+alterar_limite() {
+	
+	new_limit=$(yad --scale \
+		--title="Novo Limite" \
+		--center \
+		--width 400 \
+		--height 150 \
+		--value=$limit )
+	
+	echo $new_limit
+	echo $?
+	
+	case $? in
+		
+		0) sed -i "1s/.*/$new_limit/" $file_user ; main_aviso_usuario ;;
+		
+		1) main_aviso_usuario ;;
+	
+	esac
+	
+}
+
+
+update_percent_aviso_usuario() {
+	get_percent
+
+	limit=$(sed -n 1p $file_user)
+	usado_m=$(sed -n 2p $file_user)
+	total_m=$(sed -n 3p $file_user)
+	percent=$(sed -n 4p $file_user)
+
+
+	if [[ $percent -ge $limit ]]; then
+		aviso_excedeu="<big><big><big> $user</big><b> você está utilizando mais de $limit% \n da sua cota de disco atual!</b></big></big> \n\n"
+	else
+		aviso_excedeu=""
+	fi
+}
+
 
 
 main_aviso_usuario() {
+	update_percent_aviso_usuario
 	graph="$local_imgs/$percent"_usado.png
 	img_guardians="guardians2.jpg"
-	icon_guardians="guardiansIcon.jpg"
-	aviso_principal="<big><big><big> $user</big><b> você está utilizando mais de $limit% \n da sua cota de disco atual</b></big></big>"
-	aviso_cota_total="<big><b>- Cota Total: </b></big><big>$total_m</big>"
-	aviso_cota_usada="<big><b>- Cota Utilizada: </b></big><big>$usado_m</big>"
-	text_inform_1="<big>Você pode clicar no botão <i>Analisar Todos os Arquivos</i> \n para excluir algum arquivo ou pasta que esteja \n ocupando muito espaço.</big>"
-	text_inform_2="<big>Se quiser também, pode clicar em <i>Apagar Lixeira</i>\n ou <i>Apagar Cache</i>. Para deletar os arquivos da\n Lixeira ou Cache (Google Chrome, Firefox...).</big>"
+	icon_guardians="guardiansIcon.jpg"	
+	aviso_cota_total="<big><b>- Cota Total: </b></big><big>$total_m</big> \n\n"
+	aviso_cota_usada="<big><b>- Cota Utilizada: </b></big><big>$usado_m</big> \n\n\n"
+	text_inform_1="<big>Você pode clicar no botão <i>Analisar Todos os Arquivos</i> \n para excluir algum arquivo ou pasta que esteja \n ocupando muito espaço.</big> \n\n"
+	text_inform_2="<big>Se quiser também, pode clicar em <i>Apagar Lixeira</i>\n ou <i>Apagar Cache</i>. Para deletar os arquivos da\n Lixeira ou Cache (Google Chrome, Firefox...).</big>\n\n"
+	text_inform_3="<big>Se deseja mudar o limite em % para que essa\n mensagem apareça, clique em <i>Alterar Limite</i>.</big>"
 	
 	yad --form \
 	--title="Aviso - Guardians" \
@@ -57,10 +103,11 @@ main_aviso_usuario() {
 	--center \
 	--fixed \
 	--image "$graph" \
-	--text="$aviso_principal \n\n  $aviso_cota_total \n\n $aviso_cota_usada \n\n\n $text_inform_1 \n\n $text_inform_2" \
+	--text="$aviso_excedeu $aviso_cota_total $aviso_cota_usada $text_inform_1 $text_inform_2 $text_inform_3" \
 	--button="<b>Analisar Todos os Arquivos</b>":0 \
 	--button="<b>Limpar Lixeira</b>":2 \
 	--button="<b>Limpar Cache</b>":3 \
+	--button="<b>Alterar Limite</b>":4 \
 	--button=gtk-quit:1 
 	
 	
@@ -72,14 +119,17 @@ main_aviso_usuario() {
 # - Zenity
 # - quota
 
-cota_ultrapassou=$1
 user=$(whoami)
 home_user="/home/$user"
 lixeira_user="$home_user/.local/share/Trash/files/*"
 cache_user="$home_user/.cache/*"
 local_imgs="graficoUso"
+file_user="/home/$user/.infoCotaUser"
 
+limit=""
+usado_m=""
+total_m=""
+percent=""
+aviso_excedeu=""
 
-if [[ "$cota_ultrapassou" == "true" ]]; then
-	main_aviso_usuario
-fi
+main_aviso_usuario
--- scripts_lcc/verifica_quota/verifica_quota.sh
+++ scripts_lcc/verifica_quota/verifica_quota.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
-readonly limit=10
-aviso_usuario_bool="false"
+get_percent() {
+	user=$(whoami)
+	readonly limit_default=70
+	file_user="/home/$user/.infoCotaUser"
+	# 1 linha - limite para atingir o aviso
+	# 2 linha - cota usada
+	# 3 linha - cota total
+	# 4 linha - porcentagem
+	
+	if [ -e $file_user ]; then
+		sed -i 4d $file_user
+		sed -i 3d $file_user
+		sed -i 2d $file_user
+	else
+		echo $limit_default > $file_user
+	fi
 
-usado_m=$(quota -s | tail -1 | tr -s " " | cut -d " " -f2)
-total_m=$(quota -s | tail -1 | tr -s " " | cut -d " " -f3)
+	usado_m=$(quota -s | tail -1 | tr -s " " | cut -d " " -f2)
+	total_m=$(quota -s | tail -1 | tr -s " " | cut -d " " -f3)
+	echo $usado_m >> $file_user
+	echo $total_m >> $file_user
 
-len_usado=${#usado_m}
-len_total=${#total_m}
+	len_usado=${#usado_m}
+	len_total=${#total_m}
 
-usado=${usado_m:0:$len_usado-1}
-total=${total_m:0:$len_total-1}
+	usado=${usado_m:0:$len_usado-1}
+	total=${total_m:0:$len_total-1}
 
-percent=$(echo "scale=2; ($usado / $total) * 100" | bc | cut -d "." -f1)
+	percent=$(echo "scale=2; ($usado / $total) * 100" | bc | cut -d "." -f1)
+	echo $percent >> $file_user	
+	
+}
 
-if [[ $percent -ge $limit ]]; then
-	./aviso_usuario.sh "true"
-fi
+
+
+
+
+
+
+
