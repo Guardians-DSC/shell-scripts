f7ee38fdb46a9ed44a050a9a0df126d77bc6d346
javanlacerda <javan.lacerda@ccc.ufcg.edu.br>
2018-06-14 08:53:28 -0300

    update rstudio script

--- packages/rstudio/install.sh
+++ packages/rstudio/install.sh
@@ -15,10 +15,22 @@ apt-get -y update
 #Install gdebi
 apt-get -y install gdebi-core
 
-#Download RStudio
-wget https://download1.rstudio.org/rstudio-1.0.143-amd64.deb
+#Install plugins
+wget --tries=3 --timeout=120 http://ftp.ca.debian.org/debian/pool/main/g/gstreamer0.10/libgstreamer0.10-0_0.10.36-1.5_amd64.deb
+wget --tries=3 --timeout=120 http://ftp.ca.debian.org/debian/pool/main/g/gst-plugins-base0.10/libgstreamer-plugins-base0.10-0_0.10.36-2_amd64.deb
+sudo dpkg -i libgstreamer0.10-0_0.10.36-1.5_amd64.deb
+sudo dpkg -i libgstreamer-plugins-base0.10-0_0.10.36-2_amd64.deb
+sudo apt-mark hold libgstreamer-plugins-base0.10-0
+sudo apt-mark hold libgstreamer0.10
+
+
+#Download RStudio for Ubuntu 16.04 to Ubuntu 17.10 64bits
+
+wget --tries=3 --timeout=120 https://download1.rstudio.org/rstudio-xenial-1.1.383-amd64.deb
 
 #Install RStudio
-gdebi rstudio-1.0.143-amd64.deb
+
+sudo dpkg -i rstudio-*-amd64.deb
+
 
 rm rstudio-1.0.143-amd64.deb
